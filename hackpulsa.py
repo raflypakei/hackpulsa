2517d15fec9671b7a3b35efcdf551afc6be9fab0
raflypakei <66076381+raflypakei@users.noreply.github.com>
2020-06-03 00:12:06 +0800

    Add files via upload

---
+++ hackpulsa.py
@@ -0,0 +1,35 @@
+#!/bin/bash
+#Lumayan Buat Pamer KeKwan Lu😂
+#Tools Ini Cuman Buat Iseng² doang
+
+echo "\033[0;31m]
+      .---.        .-----------
+     /     \  __  /    ------
+    / /     \(  )/    -----
+   //////   * \/ *   ---
+  //// / // :    : ---
+ // /   /  /*    *--
+//          //..\\
+===========UU====UU==============
++          *//||\\*             +
++            ****               +
++ coded by : Raflypakei         +
++ Youtube : Rafly pake k    +
++ kontak me : 082290232340     +
++                               +
++  ^...^                 ^...^  +
++ <_* *_> TransferPulsa <_* *_> +
++   \_/                   \_/   +
+================================="
+
+echo "\033[0;32m]Masukan Nomer Anda: "
+read anda
+echo "\033[0;32m]Masukan Nomer Korban: "
+read korban
+echo "\033[0;32m]Jumblah Pulsa : "
+read pulsa
+echo
+echo "Wait"
+echo
+echo "$korban, Sukses kirimm Pulsa $pulsa"
+echo "Done BosQ"
